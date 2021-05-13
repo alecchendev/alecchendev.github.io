127a46d9679394487967447e46c2be757dd281db
Alec Chen <alecchendev@gmail.com>
2021-05-12 23:34:12 -0500

    Dark mode

--- components/layout.js
+++ components/layout.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import { getSortedPostsData } from '../lib/posts';
 import styles from '../styles/layout.module.css'
 import Link from 'next/link'
+import { useState } from 'react'
 import { FaGithub, FaTwitter } from 'react-icons/fa';
 import { MdEmail } from 'react-icons/md';
 
@@ -9,6 +10,19 @@ const name = 'Alec Chen';
 export const siteTitle = 'Alec Chen';
 
 export default function Layout({ children, home, skinny, selected }) {
+
+  const [ colorMode, setColorMode ] = useState('Dark Mode');
+
+  const toggleDarkMode = () => {
+    if (document.body.classList.contains("dark-mode")) {
+      document.body.classList.remove("dark-mode");
+      setColorMode('Dark Mode');
+    } else {
+      document.body.classList.add("dark-mode");
+      setColorMode('Light Mode');
+    }
+  }
+
   return (
     <div className={styles.container}>
 
@@ -19,6 +33,7 @@ export default function Layout({ children, home, skinny, selected }) {
         <Link href='/consumption'>Consumption</Link>
         <Link href='/people'>People</Link>
         <Link href='/blog'>Blog</Link>
+        <a onClick={toggleDarkMode}>{colorMode}</a>
       </div>
 
       <main className={styles.content}>{children}</main>
