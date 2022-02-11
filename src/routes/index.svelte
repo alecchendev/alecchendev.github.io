9237d03f67b540718c0ef9b2391a18b19112730d
Alec Chen <alecchendev@gmail.com>
2022-02-10 18:45:42 -0600

    Small changes to links and description

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,18 +7,15 @@
 	<div class='bio-box'>
 		<h1 class='header'>Hey, I'm Alec.</h1>
 		<p class='bio'>
-			Welcome to my website! I'm a second-year studying computer science and statistics at UIUC. I'll be working at Palantir this summer (2022) as an FDE intern in Washington D.C. I'm interested in cryptocurrency, currently co-founding Illini Blockchain.
+			Welcome to my website! I'm a second-year studying computer science and statistics at UIUC. I'll be working at Palantir this summer (2022) as an FDE intern in Washington D.C. I'm interested in cryptocurrency, currently co-founding Illini Blockchain, building on Solana.
 		</p>
 		<p class='links'>
 			Email: <a href="mailto:alecchendev@gmail.com" target="_blank">alecchendev@gmail.com</a>
 			<br>
-			Twitter: <a href="https://twitter.com/alecchendev" target="_blank">alecchendev</a>
-			<br>
-			Github: <a href="https://github.com/alecchendev" target="_blank">alecchendev</a>
-			<br>
-			Substack: <a href="https://alecc.substack.com/" target="_blank">Alec's Newsletter</a>
-			<br>
-			YouTube: <a href="https://www.youtube.com/channel/UCJdLjf7580_n0F_9oDtiebA" target="_blank">Alec Chen</a>
+			alecchendev on
+			<a href="https://twitter.com/alecchendev" target="_blank">Twitter</a>,
+			<a href="https://github.com/alecchendev" target="_blank">Github</a>, and
+			<a href="https://alecchendev.medium.com/" target="_blank">Medium</a>
 		</p>
 	</div>
 
@@ -67,11 +64,12 @@
 
 	a {
 		text-decoration: none;
-		color: #4c525c;
+		color: #45a0c3;
 		transition: 0.1s;
 	}
 	a:hover {
-		opacity: 0.5;
+		/* opacity: 0.5; */
+		text-decoration: underline;
 	}
 
 	@media (max-width: 778px) {
