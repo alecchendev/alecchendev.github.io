04de43f0c66f7a9065a19148a68281ef11457b93
Alec Chen <alecchendev@gmail.com>
2022-04-25 14:25:49 -0500

    Link illini blockchain

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,16 +2,18 @@
 	let name = 'world';
 </script>
 
-<div class='wrapper'>
-
-	<div class='bio-box'>
-		<h1 class='header'>Hey, I'm Alec.</h1>
-		<p class='bio'>
-			Welcome to my website! I'm a second-year studying computer science and statistics at UIUC. I'll be working at Palantir this summer (2022) as an FDE intern in Washington D.C. I'm interested in cryptocurrency, currently co-founding Illini Blockchain, building on Solana. I like to write, and occasionally I'll brave my small corner of the web and publish something.
+<div class="wrapper">
+	<div class="bio-box">
+		<h1 class="header">Hey, I'm Alec.</h1>
+		<p class="bio">
+			Welcome to my website! I'm a second-year studying computer science and statistics at UIUC.
+			I'll be working at Palantir this summer (2022) as an FDE intern in Washington D.C. I'm
+			interested in cryptocurrency, currently co-founding <a href="https://twitter.com/ILL_Blockchain" target="_blank">Illini Blockchain</a>, building on Solana. I
+			like to write, and occasionally I'll brave my small corner of the web and publish something.
 		</p>
-		<p class='links'>
+		<p class="links">
 			Email: <a href="mailto:alecchendev@gmail.com" target="_blank">alecchendev@gmail.com</a>
-			<br>
+			<br />
 			alecchendev on
 			<a href="https://twitter.com/alecchendev" target="_blank">Twitter</a>,
 			<a href="https://github.com/alecchendev" target="_blank">Github</a>, and
@@ -20,15 +22,13 @@
 	</div>
 
 	<div>
-		<div class='photo-box'>
-			<img src='/meChicago3.jpg' alt='Picture of me' class='photo' />
+		<div class="photo-box">
+			<img src="/meChicago3.jpg" alt="Picture of me" class="photo" />
 			<!-- <img src='/me.png' alt='Picture of me' class='photo' /> -->
 		</div>
-		<p class='photo-caption'>Me in Chicago</p>
+		<p class="photo-caption">Me in Chicago</p>
 	</div>
-
 </div>
-
 
 <style>
 	.wrapper {
@@ -46,7 +46,6 @@
 		width: 350px;
 	}
 	.bio {
-
 	}
 
 	.photo-box {
