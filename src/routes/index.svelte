60767d361b1ef9faf7ea457f1e3cb8fb912bc496
Alec Chen <alecchendev@gmail.com>
2022-05-03 19:15:18 -0500

    Swap photo, add encrypted email info

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,11 +8,18 @@
 		<p class="bio">
 			Welcome to my website! I'm a second-year studying computer science and statistics at UIUC.
 			I'll be working at Palantir this summer (2022) as an FDE intern in Washington D.C. I'm
-			interested in cryptocurrency, currently co-founding <a href="https://twitter.com/ILL_Blockchain" target="_blank">Illini Blockchain</a>, building on Solana. I
-			like to write, and occasionally I'll brave my small corner of the web and publish something.
+			interested in cryptocurrency, currently co-founding <a
+				href="https://twitter.com/ILL_Blockchain"
+				target="_blank">Illini Blockchain</a
+			>, building on Solana. I like to write, and occasionally I'll brave my small corner of the web
+			and publish something.
 		</p>
 		<p class="links">
-			Email: <a href="mailto:alecchendev@gmail.com" target="_blank">alecchendev@gmail.com</a>
+			Email: <a href="mailto:alecchendev@gmail.com" target="_blank">alecchendev@gmail.com</a>,
+			<a href="mailto:alecchen1@protonmail.com" target="_blank">alecchen1@protonmail.com</a> (<a
+				href="https://api.protonmail.ch/pks/lookup?op=get&search=user.email@protonmail.com"
+				>PGP key</a
+			>)
 			<br />
 			alecchendev on
 			<a href="https://twitter.com/alecchendev" target="_blank">Twitter</a>,
@@ -23,7 +30,7 @@
 
 	<div>
 		<div class="photo-box">
-			<img src="/meChicago3.jpg" alt="Picture of me" class="photo" />
+			<img src="/meChicago4.png" alt="Picture of me" class="photo" />
 			<!-- <img src='/me.png' alt='Picture of me' class='photo' /> -->
 		</div>
 		<p class="photo-caption">Me in Chicago</p>
@@ -55,23 +62,13 @@
 	}
 	.photo {
 		/* object-fit: cover; */
-		width: 150%;
-		margin: -225px 0px 0px -75px;
+		width: 180%;
+		margin: -200px 0px 0px -135px;
 		/* height: 100%; */
 	}
 	.photo-caption {
 		text-align: center;
 		margin-top: 5px;
-	}
-
-	a {
-		text-decoration: none;
-		color: #45a0c3;
-		transition: 0.1s;
-	}
-	a:hover {
-		/* opacity: 0.5; */
-		text-decoration: underline;
 	}
 
 	@media (max-width: 778px) {
